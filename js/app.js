f2f42d06c87f4f1287c382da8b88791245b67105
s14dec <shiv14dec@gmail.com>
2022-07-08 16:41:17 -0500

    Finished CSS & HTML
    finished both css and html pages

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+class Pet{
+    constructor(name, hunger, sleepiness, boredom, age){
+        this.name = name
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.boredom = boredom
+        this.age = age
+    }
+}
+
