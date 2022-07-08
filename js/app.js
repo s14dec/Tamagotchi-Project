9f6d21fc15aa00780ed438ab5026e1c6ade804f4
s14dec <shiv14dec@gmail.com>
2022-07-08 18:17:29 -0500

    hunger
    working on hunger method

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
 
+// Main Buttons
+const startButton = document.querySelector('#start')
 
+const feedButton = document.querySelector('#feed')
 
+const lightsButton = document.querySelector('#lights')
 
+const playButton = document.querySelector('#play')
 
+// Stats
+const petName = document.querySelector('#name')
 
+const hungerStat = document.querySelector('#hungerNum')
+
+const sleepinessStat = document.querySelector('#sleepinessNum')
+
+const boredomStat = document.querySelector('#boredomNum')
+
+const ageStat = document.querySelector('#ageNum')
+
+// Images
+
+const petImage = document.querySelector('#pet')
+
+const background = document.querySelector('.background')
 
 
 class Pet{
@@ -13,6 +33,53 @@ class Pet{
         this.sleepiness = sleepiness
         this.boredom = boredom
         this.age = age
+    
+    }
+
+    feedPet(){
+        this.hunger--
+    }
+
+    napTime(){
+        this.sleepiness = this.sleepiness - 5
+    }
+
+    playPet(){
+        this.boredom--
     }
 }
 
+    const tamaPet = new Pet("", 1, 1, 1, 0)
+
+    const gameTime = {
+
+        hungerManage(){
+            const hungerIncrease = setInterval(() => {
+                tamaPet.hunger++
+                hungerStat.innerHTML = tamaPet.hunger
+
+                if(tamaPet.hunger >= 10){
+                    alert(`${tamaPet.name} has perished due to Hunger`)
+                    clearInterval(hungerIncrease)
+                }
+            }, 5000)
+
+            feedButton.addEventListener("click", () => {
+                tamaPet.feedPet()
+                hungerStat.innerHTML = tamaPet.hunger
+            })
+
+        }
+        
+
+
+
+
+
+
+    }
+
+
+
+
+    gameTime.hungerManage()
